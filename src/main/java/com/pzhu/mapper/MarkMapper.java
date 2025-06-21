a30c3ce0c78d698a58bf182ad2871b4ba417c678
qing0826 <791631477@qq.com>
2025-06-21 18:16:52 +0800

    完善MarkMapper层

--- src/main/java/com/pzhu/mapper/MarkMapper.java
+++ src/main/java/com/pzhu/mapper/MarkMapper.java
@@ -11,12 +11,13 @@ import com.pzhu.bean.MarkBean;
 @Mapper
 public interface MarkMapper {
 	@Results({
-		@Result(property="mid",column="mid"),
-		@Result(property="uid",column="uid"),
-		@Result(property="cid",column="cid"),
-		@Result(property="scores",column="scores"),
+			@Result(property="mid",column="mid"),
+			@Result(property="uid",column="uid"),
+			@Result(property="cid",column="cid"),
+			@Result(property="scores",column="scores"),
 	})
 
 	//获得该uid，的cid的分数
+	@Select("SELECT * FROM mark WHERE uid=#{uid} AND cid =#{cid}")
 	MarkBean getScoresByUidCid(@Param("uid")int uid,@Param("cid")int cid);
 }
