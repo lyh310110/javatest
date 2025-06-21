9ed6d66461313925812a5fd209545d3dd0b591fe
“xixue918” <2811395908@qq.com>
2025-06-21 21:11:11 +0800

    完善了SelectTimemapper层的具体实现功能

--- src/main/java/com/pzhu/mapper/SelectTimeMapper.java
+++ src/main/java/com/pzhu/mapper/SelectTimeMapper.java
@@ -9,6 +9,7 @@ import org.apache.ibatis.annotations.Update;
 
 import com.pzhu.bean.SelectTime;
 
+
 @Mapper
 public interface SelectTimeMapper {
 	@Results({
@@ -16,8 +17,12 @@ public interface SelectTimeMapper {
 		@Result(property="starttime",column="starttime"),
 		@Result(property="endtime",column="endtime")
 	})
-	
+
+	@Select("select * from select_time")
+		//查询 select_time 表的第一条记录
 	SelectTime getSelectTime();
-	
+
+//更新 select_time 表中 sid 为 1 的记录的开始时间和结束时间
+	@Update("UPDATE `select_time` SET `starttime`=#{starttime}, `endtime`=#{endtime} WHERE (`sid`='1')")
 	boolean setSelectTime(@Param("starttime")java.sql.Date starttime,@Param("endtime")java.sql.Date endtime);
 }
